58c33e2108eae78ad92f0467d2802f53b5ce6c5b
jingyang <linshuboy@qq.com>
2020-10-09 15:38:13 +0800

    init

--- src/HukeServiceProvider.php
+++ src/HukeServiceProvider.php
@@ -37,6 +37,7 @@ class HukeServiceProvider extends ServiceProvider
     {
         $configPath = realpath(__DIR__ . '/../config/huke.php');
         $this->mergeConfigFrom($configPath, 'huke');
+        $this->publishes([$configPath => config_path('huke.php')], 'config');
     }
 
 
